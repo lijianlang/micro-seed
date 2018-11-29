78343965624df51798fd120d3632a40acc0b6723
lijianlang <Lijl123>
2018-11-29 18:19:16 +0800

    集成freemarker

--- demo/src/main/java/cn/ljl/demo/controller/DemoController.java
+++ demo/src/main/java/cn/ljl/demo/controller/DemoController.java
@@ -3,10 +3,13 @@ package cn.ljl.demo.controller;
 import cn.ljl.demo.domain.Product;
 import cn.ljl.demo.mapper.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class DemoController {
 
     @Autowired
@@ -22,5 +25,11 @@ public class DemoController {
     public void testMyBatis() {
         Product product = productMapper.selectByPrimaryKey();
         System.out.println(product);
+    }
+
+    @GetMapping(value = "/login")
+    public String login(Model model) {
+        model.addAttribute("name", "lily");
+        return "login";
     }
 }
